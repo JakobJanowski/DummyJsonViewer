27b7c572b9444112c180e5575dbb41cf6ebc372f
Jakub Janowski <jjacub.765@gmail.com>
2026-01-07 20:56:29 +0000

    Added some error handling and expanded comments

--- DummyJsonViewer/Controllers/ProductsController.cs
+++ DummyJsonViewer/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 
 namespace DummyJsonViewer.Controllers
 {
+    
     public class ProductsController : Controller
     {
         const string BASE_URL = "https://dummyjson.com/products";
@@ -20,21 +21,22 @@ namespace DummyJsonViewer.Controllers
             _logger = logger;
             _clientFactory = clientFactory;
         }
-       
 
+        /* Method used to get a array of products for the index page
+        * Limit - How many products should be listed on each page, unused but functionality exists to modify if needed
+        * PageNum - The current page number, used to determine which set of products to get
+        * 
+        * Returns - A Products object which is an array of Product, The page current Page number, number of pages and total products are put in the ViewBag
+        */
         public async Task<IActionResult> Index(int? Limit,int? PageNum)
         {
-            //Calculate skip
-            int limit = Limit ?? 30;
            
-            int pageNum = PageNum ?? 0;
-            if(pageNum < 0)
-            {
-                pageNum = 0;
-            }
+            //Again Limit is currently unused, if you want to use it then edit this part
+            int limit = Limit ?? 30;
 
-            int totalPages = 0;
             //Figure out total pages and total products
+            int totalPages = 0;
+         
             var message = new HttpRequestMessage();
             message.Method = HttpMethod.Get;
             message.RequestUri = new Uri($"{BASE_URL}?limit=0");
@@ -56,12 +58,26 @@ namespace DummyJsonViewer.Controllers
                 ViewBag.TotalPages = totalPages;
 
             }
+            else
+            {
+                //No products have been found
+                //No products found return nothing
+                return View(new Products());
+            }
+
+            //Figure out current Page Number
+            int pageNum = PageNum ?? 0;
+            if (pageNum < 0)
+            {
+                pageNum = 0;
+            }
 
             if (totalPages < pageNum)
             {
                 pageNum = totalPages;
             }
 
+            //Make request for products to be displayed
             message = new HttpRequestMessage();
             message.Method = HttpMethod.Get;
             message.RequestUri = new Uri($"{BASE_URL}?limit={limit}&skip={pageNum*limit}"); 
@@ -75,28 +91,30 @@ namespace DummyJsonViewer.Controllers
             {
               
                 using var responseStream = await response.Content.ReadAsStreamAsync();
-              
                 products = await JsonSerializer.DeserializeAsync<Products>(responseStream);
 
             }
             else
             {
-                _logger.LogInformation("Not Succsess");
-               
-                
-                
+                //No products found return nothing
+                return View(new Products());
             }
-
-            
-
 
             ViewBag.PageNum = pageNum;
 
             return View(products);
         }
 
+        /*
+        * Method used for the detail page to load more details about a certain product
+        * id - the id of the Product in question. Primary Key
+        * PageNum - the page this product was on, used solely for the go back button and is not changed in this method
+        * 
+        * Returns - The individual product that was searched for. Pagenum is passed straight back into the viewbag
+        */
         public async Task<IActionResult> Detail(string id,int PageNum)
         {
+            
             if (id == null)
                 return NotFound();
 
@@ -116,7 +134,7 @@ namespace DummyJsonViewer.Controllers
                 using var responseStream = await response.Content.ReadAsStreamAsync();
                 product = await JsonSerializer.DeserializeAsync<Product>(responseStream);
             }
-           
+            
 
             if (product == null)
                 return NotFound();
--- DummyJsonViewer/Models/Products.cs
+++ DummyJsonViewer/Models/Products.cs
@@ -2,6 +2,9 @@
 
 namespace DummyJsonViewer.Models
 {
+    //When doing a call to https://dummyjson.com/products it will give the products in a array also called products
+    //As in "products": [...] instead of [...]
+    //Therefore for this call we need this to store the resulting json
     public class Products
     {
         [Required]
